90be115f09a7de2643f12e1006d01891e97d7ee7
izasoerya <ihza.surya@gmail.com>
2024-01-01 12:26:35 +0700

    feat: add roll dice

---
+++ lib/roll_dice.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DiceRoll extends StatefulWidget {
+  const DiceRoll(key) : super(key: key);
+  @override
+  State<DiceRoll> createState() => _DiceRoll();
+}
+
+class _DiceRoll extends State<DiceRoll> {
+  var assetImage = 'assets/images/dice-1.png';
+  void rollDice() {
+    setState(() {
+      assetImage = 'assets/images/dice-4.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          assetImage,
+          width: 250,
+        ),
+        TextButton(onPressed: rollDice, child: const Text('Re-Roll'))
+      ],
+    ));
+  }
+}
